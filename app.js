45a03a03e73edb3276a79468d489553cf8d37eeb
Nagaruuuu <letronghoa050595@gmail.com>
2024-03-19 17:54:46 +0700

    first commit

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ Vue.createApp({
     data() {
         return {
             name: 'Hoa',
-            age: '29'
+            age: 29,
+            image: ""
         }
     },
-    methods() {
-        
+    methods: {
+        calculatorAge() {
+            return this.age + 5;
+        },
+
+        randomNumber() {
+            return Math.random();
+        }
     }
 }).mount('#assignment');
