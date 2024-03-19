4511c87b44a2691a69d0ab6df6e65f51c4680838
TrongHoa <34540606+Nagaruu@users.noreply.github.com>
2024-03-20 00:24:01 +0700

    Done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ Vue.createApp({
         return {
             name: 'Hoa',
             age: 29,
-            image: ""
+            image: "https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/429966781_2115931472090245_8258911117600734339_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=U2c--3m5gFEAX9Ej1_P&_nc_ht=scontent.fhan17-1.fna&oh=00_AfAqGyPyEnaat1d8TTyNe5J1r2fM94vIHps_HuGbxK8Paw&oe=65FEA055"
         }
     },
     methods: {
